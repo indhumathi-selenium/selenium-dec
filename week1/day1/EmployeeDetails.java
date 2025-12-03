1cae759caf3729c0c1ee054f2ef000fbe62316b7
indhumathi-selenium <indhu.ayser@gmail.com>
2025-12-03 20:33:30 +0300

    Add files via upload
    week1 day1 changes

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class EmployeeDetails {
+public static void main(String[] args) {
+	
+	String name = "John Doe";
+	int age = 30;
+	Double salary = 55000.75;
+	char gender = 'M';
+	boolean accept_terms = true;
+	float rating = 4.5f;
+	String mobile_number = "9876543210";
+	int pincode= 56001;
+	int lucky_number= 7;
+	
+	System.out.println("Name:"+ name );
+	System.out.println("age:"+ age);
+	System.out.println("salary;"+ salary);
+	System.out.println("Gender:" +gender);
+	System.out.println("Terms:" +accept_terms);
+	System.out.println("Rating:" +rating);
+	System.out.println("Mobile Number:" +mobile_number);
+	System.out.println("Pincode:" + pincode);
+	System.out.println("Lucky Number" + lucky_number);
+	
+	
+			
+	
+	
+	
+}
+}
---
+++ week1/day1/Hellotestleaf.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Hellotestleaf {
+public static void main(String[] args) {
+	System.out.println("welcome to testleaf");
+
+	int month = 12;
+	char day = 'w';
+	String Month = "December";
+	boolean M = true;
+	float number= 1.000909f;
+	
+	System.out.println(month);
+	System.out.println(day);
+	System.out.println(month);
+	System.out.println(M);
+	System.out.println(number);
+} 
+}
+
